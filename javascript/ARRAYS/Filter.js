72307fcc7e9b5591d45ceda4845be80c571287a5
Elie237 <eliemigisha5@gmail.com>
2022-02-23 11:22:48 +0300

    :100:

--- javascript/ARRAYS/Filter.js
+++ javascript/ARRAYS/Filter.js
@@ -1,16 +1,16 @@
-// let food = [
-// {name: 'chicken',price: 450},
-// {name:'pilau',price:400},
-// {name:'chips',price:200},
-// ];
+let food = [
+{name: 'chicken',price: 450},
+{name:'pilau',price:400},
+{name:'chips',price:200},
+];
 let price = [400,450,300,200]
 let expensiveFoods=price.filter((check) => {return check > 200})
 console.log(expensiveFoods);
 
-let food = [
+let foods =[
     {name: 'chicken',price: 450},
     {name: 'pilau',price:400},
     {name: 'chips',price:200},
     ];
-let filteredFoods = food.filter((check) => {return check.price > 200})
+let filteredFoods = foods.filter((check) => {return check.price > 200})
 console.log(filteredFoods);    
--- javascript/ARRAYS/Reduce.js
+++ javascript/ARRAYS/Reduce.js
@@ -4,18 +4,17 @@ let food = [
     {name: 'chips',price:200},
     ];
 let price = [400,450,300,200]
-let totalprice = price.reduce((prev,next) => 
-{
-    return prev+next
-});
+let totalprice = price.reduce((prev,next) => {return prev+next});
 console.log(totalprice);    
 
 
-let food = [
-    {name: 'chicken',price: 450},
-    {name: 'pilau',price:400},
-    {name: 'chips',price:200},
-    ];
-let price = [400,450,300,200]
-let totalprice = price.reduce((check) => {return check.price > 200});
-console.log(totalprice);    
+// let food = [
+//     {name: 'chicken',price: 450},
+//     {name: 'pilau',price:400},
+//     {name: 'chips',price:200},
+//     ];
+// let price = [400,450,300,200]
+// let totalprice = price.reduce((check) => {return check.price > 200});
+// console.log(totalprice); 
+
+
